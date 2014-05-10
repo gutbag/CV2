a1191549d20b44556e64c626ee6dae613920631f
Mark Lamb <git@gutbag.com>
2014-05-10 23:25:12 +0100

    Moved pin defs to header

---
+++ CV2/CV2.h
@@ -0,0 +1,31 @@
+#ifndef CV2_CV2_h
+#define CV2_CV2_h
+
+#define OPTO_1 24
+#define OPTO_2 25
+#define OPTO_3 22
+#define OPTO_4 15
+#define OPTO_5 16
+#define OPTO_6 17
+
+#define FSW_1 46
+#define FSW_2 47
+#define FSW_3 48
+#define FSW_LED1 36
+#define FSW_LED2 34
+#define FSW_LED3 32
+
+#define PATCH_DOWN_SW 52
+#define PATCH_UP_SW 53
+
+#define D_SPARE_1 49
+#define D_SPARE_2 50
+#define D_SPARE_3 51
+
+#define CS_PIN_5V_DAC 10
+#define CS_PIN_9V_DAC 4
+
+#define EXPR_1_PIN A3
+#define EXPR_2_PIN A11
+
+#endif
