64c049f6893ef56d95271e2aca6ef001354bc831
Mark Lamb <git@gutbag.com>
2014-05-22 20:19:24 +0100

    Completed basic impl.

--- CV2/SwitchTrigger.cpp
+++ CV2/SwitchTrigger.cpp
@@ -2,7 +2,7 @@
 #include "Switch.h"
 
 SwitchTrigger::SwitchTrigger(Switch& aSwitch)
-: theSwitch(aSwitch), triggeredState(false)
+: theSwitch(aSwitch), triggeredState(false), switchOn(false), triggerOffToOnState(false)
 {
 	
 }
@@ -15,20 +15,41 @@ SwitchTrigger::~SwitchTrigger()
 void SwitchTrigger::setup()
 {
 	theSwitch.setup();
+	switchOn = theSwitch.isOn();
 }
 
 void SwitchTrigger::loop(const unsigned long usNow)
 {
 	theSwitch.loop(usNow);
+	boolean newSwitchOn = theSwitch.isOn();
 	
-	if (theSwitch.isOn())
-		triggeredState = true;
+	if (triggerOffToOnState)
+	{
+		if ( ! switchOn && newSwitchOn)
+			triggeredState = true;
+		else
+			triggeredState = false;
+	}
 	else
-		triggeredState = false;
-		
+	{
+		if (switchOn &&  ! newSwitchOn)
+			triggeredState = true;
+		else
+			triggeredState = false;
+	}
+	
+	switchOn = newSwitchOn;
 }
 
+// returns triggered state and clears it
 boolean SwitchTrigger::triggered()
 {
-	return triggeredState;
+	boolean returnState = triggeredState;
+	triggeredState = false;
+	return returnState;
+}
+
+void SwitchTrigger::triggerOnToOff(const boolean state)
+{
+	triggerOffToOnState = ! state;
 }
--- CV2/SwitchTrigger.h
+++ CV2/SwitchTrigger.h
@@ -11,12 +11,15 @@ class SwitchTrigger : public Trigger
 public:
 	SwitchTrigger(Switch& aSwitch);
 	virtual ~SwitchTrigger();
+	void triggerOnToOff(const boolean state);
 	void setup();
 	void loop(const unsigned long usNow);
 	boolean triggered();
 private:
 	Switch& theSwitch;
 	boolean triggeredState;
+	boolean switchOn;
+	boolean triggerOffToOnState;
 };
 
 #endif /* defined(__CV2__SwitchTrigger__) */
