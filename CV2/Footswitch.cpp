4f1a1496798ae5e4ca181b6774de4bee5a033c81
Mark Lamb <git@gutbag.com>
2014-07-16 22:27:13 +0100

    Made footswitch triggerable

--- CV2/Footswitch.cpp
+++ CV2/Footswitch.cpp
@@ -1,4 +1,5 @@
 #include "Footswitch.h"
+#include "MIDI.h"
 
 static Footswitch* pInstances[3] = {NULL, NULL, NULL};
 
@@ -7,8 +8,8 @@ Footswitch& Footswitch::instance(const uint8_t index)
 	return *pInstances[index];
 }
 
-Footswitch::Footswitch(const uint8_t instanceIndex, const uint8_t aSwPin, const uint8_t anLedPin)
-: Switch(aSwPin), ledPin(anLedPin)
+Footswitch::Footswitch(const uint8_t instanceIndex, const uint8_t aMidiChannel, const uint8_t aSwPin, const uint8_t anLedPin)
+: Switch(aSwPin), ledPin(anLedPin), midiChannel(aMidiChannel), onOffInstanceNumber(0), pOnOff(NULL), ledIsOn(false)
 {
 	pInstances[instanceIndex] = this;
 }
@@ -23,10 +24,61 @@ void Footswitch::setup()
 	Switch::setup();
 	
 	pinMode(ledPin, OUTPUT);
-	digitalWrite(ledPin, LOW);
+	setLed(false, true);
+
+	MIDI::instance().setCCListener(this, midiChannel, FOOTSWITCH_TRIGGER_INSTANCE_CC);
 }
 
 void Footswitch::loop(const unsigned long usNow)
 {
 	Switch::loop(usNow);
+	
+	if (pOnOff != NULL)
+	{
+		setLed(pOnOff->isOn());
+	}
+}
+
+void Footswitch::setLed(const boolean on, const boolean force /*=false*/)
+{
+	if (force || (ledIsOn != on))
+	{
+		digitalWrite(ledPin, on ? HIGH : LOW);
+		ledIsOn = on;
+	}
+}
+
+void Footswitch::processCCMessage(const uint8_t channel,
+							const uint8_t controllerNumber,
+							const uint8_t value)
+{
+	//	Serial.print("Footswitch Message, ch: ");
+	//	Serial.print(channel, DEC);
+	//	Serial.print(" No: ");
+	//	Serial.print(controllerNumber, DEC);
+	//	Serial.print(" Val: ");
+	//	Serial.println(value, DEC);
+	
+	switch (controllerNumber)
+	{
+		case FOOTSWITCH_TRIGGER_INSTANCE_CC:
+			onOffInstanceNumber = value;
+			pOnOff = NULL;
+			if (onOffInstanceNumber > 0)
+				pOnOff = &TriggeredOnOff::instance(onOffInstanceNumber - 1);
+			break;
+		default:
+			break;
+	}
+}
+
+uint8_t Footswitch::getControllerValue(const uint8_t controllerNumber)
+{
+	switch (controllerNumber)
+	{
+		case FOOTSWITCH_TRIGGER_INSTANCE_CC:
+			return onOffInstanceNumber;
+		default:
+			return DO_NOT_SAVE_VALUE;
+	}
 }
--- CV2/Footswitch.h
+++ CV2/Footswitch.h
@@ -3,17 +3,28 @@
 
 #include <Arduino.h>
 #include "Switch.h"
+#include "TriggeredOnOff.h"
+#include "MIDICCListener.h"
 
-class Footswitch : public Switch
+class Footswitch : public Switch, public MIDICCListener
 {
 public:
 	static Footswitch& instance(const uint8_t index);
-	Footswitch(const uint8_t instanceIndex, const uint8_t aSwPin, const uint8_t anLedPin);
+	Footswitch(const uint8_t instanceIndex, const uint8_t aMidiChannel, const uint8_t aSwPin, const uint8_t anLedPin);
 	virtual ~Footswitch();
 	void setup();
 	void loop(const unsigned long usNow);
+	void processCCMessage(const uint8_t channel,
+						  const uint8_t controllerNumber,
+						  const uint8_t value);
+	uint8_t getControllerValue(const uint8_t controllerNumber);
 private:
+	void setLed(const boolean on, const boolean force=false);
 	uint8_t ledPin;
+	uint8_t midiChannel;
+	uint8_t onOffInstanceNumber;
+	TriggeredOnOff* pOnOff;
+	boolean ledIsOn;
 };
 
 #endif /* defined(__CV2__Footswitch__) */
