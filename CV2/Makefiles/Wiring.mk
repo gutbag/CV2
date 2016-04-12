281eaceba5c27a089700cb22943ea261f66444c6
Mark Lamb <git@gutbag.com>
2016-04-12 22:03:21 +0100

    Updated embedxcode

--- CV2/Makefiles/Wiring.mk
+++ CV2/Makefiles/Wiring.mk
@@ -3,22 +3,22 @@
 # ----------------------------------
 # Embedded Computing on Xcode
 #
-# Copyright © Rei VILO, 2010-2014
+# Copyright © Rei VILO, 2010-2016
 # http://embedxcode.weebly.com
 # All rights reserved
 #
 #
-# Last update: Mar 01, 2014 release 136
+# Last update: Dec 09, 2015 release 4.0.7
 
 # WIRING SUPPORT IS PUT ON HOLD
-WARNING_MESSAGE = 'WIRING SUPPORT IS PUT ON HOLD'
+#WARNING_MESSAGE = Support for Wiring is put on hold.
 
 
 # Wiring specifics
 # ----------------------------------
 #
 PLATFORM         := Wiring
-PLATFORM_TAG      = WIRING=100 EMBEDXCODE=$(RELEASE_NOW)
+PLATFORM_TAG      = WIRING=101 EMBEDXCODE=$(RELEASE_NOW)
 APPLICATION_PATH := $(WIRING_PATH)
 
 APP_TOOLS_PATH   := $(APPLICATION_PATH)/tools/avr/bin
@@ -26,16 +26,25 @@ CORE_LIB_PATH    := $(APPLICATION_PATH)/cores/Common
 APP_LIB_PATH     := $(APPLICATION_PATH)/libraries
 BOARDS_TXT       := $(APPLICATION_PATH)/hardware/Wiring/boards.txt
 
+# Version check
+#
+w001 = $(APPLICATION_PATH)/lib/version.txt
+VERSION_CHECK = $(shell if [ -f $(w001) ] ; then cat $(w001) ; fi)
+ifneq ($(VERSION_CHECK),v1.0.1-dev)
+    $(error Wiring release 1.0.1 required.)
+endif
+
+
 # Sketchbook/Libraries path
 # wildcard required for ~ management
 # ?ibraries required for libraries and Libraries
 #
-ifeq ($(USER_PATH)/Library/Wiring/preferences.txt,)
+ifeq ($(USER_LIBRARY_DIR)/Wiring/preferences.txt,)
     $(error Error: run Wiring once and define the sketchbook path)
 endif
 
 ifeq ($(wildcard $(SKETCHBOOK_DIR)),)
-    SKETCHBOOK_DIR = $(shell grep sketchbook.path $(USER_PATH)/Library/Wiring/preferences.txt | cut -d = -f 2)
+    SKETCHBOOK_DIR = $(shell grep sketchbook.path $(USER_LIBRARY_DIR)/Wiring/preferences.txt | cut -d = -f 2)
 endif
 ifeq ($(wildcard $(SKETCHBOOK_DIR)),)
     $(error Error: sketchbook path not found)
@@ -74,30 +83,26 @@ BUILD_CORE_LIBS_LIST = $(subst .h,,$(subst $(BUILD_CORE_LIB_PATH)/,,$(wildcard $
 
 BUILD_CORE_C_SRCS    = $(wildcard $(BUILD_CORE_LIB_PATH)/*.c) # */
 
-ifneq ($(strip $(NO_CORE_MAIN_FUNCTION)),)
-    BUILD_CORE_CPP_SRCS = $(filter-out %program.cpp %main.cpp,$(wildcard $(BUILD_CORE_LIB_PATH)/*.cpp)) # */
-else
-    BUILD_CORE_CPP_SRCS = $(filter-out %program.cpp, $(wildcard $(BUILD_CORE_LIB_PATH)/*.cpp)) # */
-endif
+BUILD_CORE_CPP_SRCS  = $(filter-out %program.cpp %main.cpp,$(wildcard $(BUILD_CORE_LIB_PATH)/*.cpp)) # */
 
-BUILD_CORE_OBJ_FILES  = $(BUILD_CORE_C_SRCS:.c=.o) $(BUILD_CORE_CPP_SRCS:.cpp=.o)
-BUILD_CORE_OBJS       = $(patsubst $(BUILD_CORE_LIB_PATH)/%,$(OBJDIR)/%,$(BUILD_CORE_OBJ_FILES))
+BUILD_CORE_OBJ_FILES  = $(BUILD_CORE_C_SRCS:.c=.c.o) $(BUILD_CORE_CPP_SRCS:.cpp=.cpp.o)
+BUILD_CORE_OBJS       = $(patsubst $(APPLICATION_PATH)/%,$(OBJDIR)/%,$(BUILD_CORE_OBJ_FILES))
 
 # Extra variant library
 #
-VARIANT_CPP_SRC = $(wildcard $(VARIANT_PATH)/*.cpp)
-VARIANT_OBJS    = $(patsubst $(VARIANT_PATH)/%.cpp,$(OBJDIR)/libs/%.o,$(VARIANT_CPP_SRC)) # */
+VARIANT_CPP_SRC = $(wildcard $(VARIANT_PATH)/*.cpp) # */
+VARIANT_OBJS    = $(patsubst $(APPLICATION_PATH)/%.cpp,$(OBJDIR)/%.cpp.o,$(VARIANT_CPP_SRC)) # */
 
 # Two locations for Wiring libraries
 #
 BUILD_APP_LIB_PATH = $(BUILD_CORE_LIB_PATH)/libraries
 
 ifndef APP_LIBS_LIST
-    w1             = $(realpath $(sort $(dir $(wildcard $(APP_LIB_PATH)/*/*.h $(APP_LIB_PATH)/*/*/*.h)))) # */
-    APP_LIBS_LIST  = $(subst $(APP_LIB_PATH)/,,$(filter-out $(EXCLUDE_LIST),$(w1)))
+    w101             = $(realpath $(sort $(dir $(wildcard $(APP_LIB_PATH)/*/*.h $(APP_LIB_PATH)/*/*/*.h)))) # */
+    APP_LIBS_LIST    = $(subst $(APP_LIB_PATH)/,,$(filter-out $(EXCLUDE_LIST),$(w101)))
 
-    w2             = $(realpath $(sort $(dir $(wildcard $(BUILD_APP_LIB_PATH)/*/*.h $(BUILD_APP_LIB_PATH)/*/*/*.h)))) # */
-    BUILD_APP_LIBS_LIST = $(subst $(BUILD_APP_LIB_PATH)/,,$(filter-out $(EXCLUDE_LIST),$(w2)))
+    w102             = $(realpath $(sort $(dir $(wildcard $(BUILD_APP_LIB_PATH)/*/*.h $(BUILD_APP_LIB_PATH)/*/*/*.h)))) # */
+    BUILD_APP_LIBS_LIST = $(subst $(BUILD_APP_LIB_PATH)/,,$(filter-out $(EXCLUDE_LIST),$(w102)))
 else
     BUILD_APP_LIBS_LIST = $(APP_LIBS_LIST)
 endif
@@ -106,16 +111,18 @@ ifneq ($(APP_LIBS_LIST),0)
     APP_LIBS        = $(patsubst %,$(APP_LIB_PATH)/%,$(APP_LIBS_LIST))
     APP_LIB_CPP_SRC = $(wildcard $(patsubst %,%/*.cpp,$(APP_LIBS))) # */
     APP_LIB_C_SRC   = $(wildcard $(patsubst %,%/*.c,$(APP_LIBS))) # */
+    APP_LIB_H_SRC   = $(wildcard $(patsubst %,%/*.h,$(APP_LIBS))) # */
 
-    APP_LIB_OBJS    = $(patsubst $(APP_LIB_PATH)/%.cpp,$(OBJDIR)/libs/%.o,$(APP_LIB_CPP_SRC))
-    APP_LIB_OBJS   += $(patsubst $(APP_LIB_PATH)/%.c,$(OBJDIR)/libs/%.o,$(APP_LIB_C_SRC))
+    APP_LIB_OBJS    = $(patsubst $(APPLICATION_PATH)/%.cpp,$(OBJDIR)/%.cpp.o,$(APP_LIB_CPP_SRC))
+    APP_LIB_OBJS   += $(patsubst $(APPLICATION_PATH)/%.c,$(OBJDIR)/%.c.o,$(APP_LIB_C_SRC))
 
     BUILD_APP_LIBS        = $(patsubst %,$(BUILD_APP_LIB_PATH)/%,$(BUILD_APP_LIBS_LIST))
     BUILD_APP_LIB_CPP_SRC = $(wildcard $(patsubst %,%/*.cpp,$(BUILD_APP_LIBS))) # */
     BUILD_APP_LIB_C_SRC   = $(wildcard $(patsubst %,%/*.c,$(BUILD_APP_LIBS))) # */
+    BUILD_APP_LIB_H_SRC   = $(wildcard $(patsubst %,%/*.h,$(BUILD_APP_LIBS))) # */
 
-    BUILD_APP_LIB_OBJS    = $(patsubst $(BUILD_APP_LIB_PATH)/%.cpp,$(OBJDIR)/libs/%.o,$(BUILD_APP_LIB_CPP_SRC))
-    BUILD_APP_LIB_OBJS   += $(patsubst $(BUILD_APP_LIB_PATH)/%.c,$(OBJDIR)/libs/%.o,$(BUILD_APP_LIB_C_SRC))
+    BUILD_APP_LIB_OBJS    = $(patsubst $(APPLICATION_PATH)/%.cpp,$(OBJDIR)/%.cpp.o,$(BUILD_APP_LIB_CPP_SRC))
+    BUILD_APP_LIB_OBJS   += $(patsubst $(APPLICATION_PATH)/%.c,$(OBJDIR)/%.c.o,$(BUILD_APP_LIB_C_SRC))
 endif
 
 MCU_FLAG_NAME  = mmcu
